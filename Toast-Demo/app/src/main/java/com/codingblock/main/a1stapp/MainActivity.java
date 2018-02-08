76b035aa7f08528bea014cc9ebdbbb69fda111f5
Kartik Gupta <kartikgupta676@gmail.com>
2018-02-08 14:44:50 +0530

    How to use toast in Android

---
+++ Toast-Demo/app/src/main/java/com/codingblock/main/a1stapp/MainActivity.java
@@ -0,0 +1,52 @@
+package com.codingblock.main.a1stapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    public static final String TAG = "MainActivity";
+    static int count = 0;
+    TextView textView1;
+    EditText editText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        if (savedInstanceState!=null && savedInstanceState.containsKey("COUNT")){
+            count = savedInstanceState.getInt("COUNT");
+
+        }
+        Log.d(TAG, "onCreate: ");
+        textView1 = (TextView) findViewById(R.id.textView);
+        editText = (EditText) findViewById(R.id.editText2);
+    }
+
+    public void incrementCount(View view) {
+        count++;
+        Log.d(TAG, "incrementCount: was called " + count + " times");
+        textView1.setText(String.valueOf(count));
+
+        Toast.makeText(this,"The current value of count is " + count,Toast.LENGTH_SHORT).show();
+
+//        Button b = (Button) view;
+//
+//        b.getText();
+//
+//        String etContent = editText.getText().toString();
+//        textView1.append(etContent);
+
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putInt("COUNT",count);
+    }
+}
---
+++ Toast-Demo/app/src/main/java/com/codingblock/main/a1stapp/MyView.java
@@ -0,0 +1,26 @@
+package com.codingblock.main.a1stapp;
+
+import android.content.Context;
+import android.support.annotation.Nullable;
+
+        import android.content.Context;
+        import android.support.annotation.Nullable;
+        import android.util.AttributeSet;
+
+
+public class MyView extends android.support.v7.widget.AppCompatTextView {
+
+    public MyView(Context context) {
+        super(context);
+    }
+
+    public MyView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public MyView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+
+}
